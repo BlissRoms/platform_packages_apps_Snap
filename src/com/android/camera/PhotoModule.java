CyanogenMod/android_packages_apps_Gallery2

camera: Add parameter debugging support
Change-Id: Ifc1653f0046af169ba36d94796b4d9a3055c6798

src/com/android/camera/PhotoModule.java
@@ -1993,6 +1993,7 @@ public class PhotoModule
             updateCameraParametersPreference();
         }
 
+        Util.dumpParameters(mParameters);
         mCameraDevice.setParameters(mParameters);
     }
 

src/com/android/camera/VideoModule.java
@@ -1936,6 +1936,8 @@ public class VideoModule implements CameraModule,
                 CameraProfile.QUALITY_HIGH);
         mParameters.setJpegQuality(jpegQuality);
 
+        Util.dumpParameters(mParameters);
+
         mActivity.mCameraDevice.setParameters(mParameters);
         // Keep preview size up to date.
         mParameters = mActivity.mCameraDevice.getParameters();

BlissRoms/platform_packages_apps_Snap

src/com/android/camera/PhotoModule.java
@@ -4237,6 +4237,7 @@ public class PhotoModule
                 doModeSwitch = updateCameraParametersPreference();
             }
 
+            CameraUtil.dumpParameters(mParameters);
             mCameraDevice.setParameters(mParameters);
 
             // Switch to gcam module if HDR+ was selected

src/com/android/camera/VideoModule.java
@@ -2803,6 +2803,9 @@ public class VideoModule implements CameraModule,
         int jpegQuality = CameraProfile.getJpegEncodingQualityParameter(mCameraId,
                 CameraProfile.QUALITY_HIGH);
         mParameters.setJpegQuality(jpegQuality);
+
+        CameraUtil.dumpParameters(mParameters);
+
         //Call Qcom related Camera Parameters
         qcomSetCameraParameters();
 