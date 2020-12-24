LineageOS/android_packages_apps_Snap

Snap: Add privapp permissions xml
Change-Id: I46b29c519f98d56eea3057f73e9411f9ea9a2340

Android.mk
@@ -60,7 +60,17 @@ else
   LOCAL_REQUIRED_MODULES := libjni_snapmosaic libjni_snaptinyplanet libjni_snapimageutil
 endif
 
+LOCAL_REQUIRED_MODULES += privapp_whitelist_org.lineageos.snap.xml
+
 include $(BUILD_PACKAGE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := privapp_whitelist_org.lineageos.snap.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 

BlissRoms/platform_packages_apps_Snap

Android.mk
@@ -61,7 +61,18 @@ else
   LOCAL_REQUIRED_MODULES := libjni_snapmosaic libjni_snaptinyplanet libjni_snapimageutil
 endif
 
+LOCAL_REQUIRED_MODULES += privapp_whitelist_org.lineageos.snap.xml
+
 include $(BUILD_PACKAGE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := privapp_whitelist_org.lineageos.snap.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_SYSTEM_EXT_ETC)/permissions
+LOCAL_SYSTEM_EXT_MODULE := true
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 